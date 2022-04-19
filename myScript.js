83d2afa6a5d3af107cea20a9b4d2461268e92c97
Abbott-4 <103395263+Abbott-4@users.noreply.github.com>
2022-04-19 11:04:41 +0800

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,7 @@
+function myFunction(){
+    var x = document.getElementById("myText").value;
+    var y = document.getElementById("Text").value;
+    var z= Number(x) + Number(y);
+    document.getElementById("demo").innerHTML = z ;
+    }
+//Abbott（Lin Jixiang)  Brief description of this JavaScript:Realize the addition result of two numbers, so that the input can only be numbers, not numbers, and an error message will pop up
